75156f803839b6aa992e73ba65e178b52a3e58de
Nate Trichanh <natetrichanh@gmail.com>
2022-09-08 21:19:53 -0700

    Navbar & Login body

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import Logo from "./static/logo.jpg";
 import "./static/styles.css"
 
@@ -6,8 +8,12 @@ import "./static/styles.css"
 const Navbar = () => {
 
     return (
-        <div>
+        <div className="navbar-container">
             <img src={Logo} alt="logo" className="navbar-logo"/>
+            <div className="navbar-links">
+                <Link to='/events' className="navbar-link">Events</Link>
+                <Link to='/Agenda' className="navbar-link">Agenda</Link>
+            </div>
         </div>
     )
 }
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -6,6 +6,12 @@ const Login = () => {
     return (
         <div>
             <Navbar/>
+            <div className="container">
+                <div className="login-header-container">
+                    <h1 className="login-header"><span className="blue">Mora</span></h1>
+                    <h1 className="login-header">Public Health Days</h1>
+                </div>
+            </div>
         </div>
     )
 }
