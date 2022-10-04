6573b9362b3ca2741cf0235c270979d4659d64d5
Nate Trichanh <43255617+natetrichanhle@users.noreply.github.com>
2022-10-04 16:37:32 -0700

    Add files via upload

---
+++ server/routes/events.routes.js
@@ -0,0 +1,8 @@
+// const EventsController = require('./controllers/events.controller');
+// module.exports = function (app) {
+//     app.get('/api/events', EventsController.getEvents);
+//     app.post('/api/events/new', EventsController.createEvents);
+//     app.get('/api/events/:id', EventsController.getOneEvents);
+//     app.put('/api/events/edit/:id', EventsController.updateEvents);
+//     app.delete('/api/events/:id', EventsController.deleteEvents);
+// }
